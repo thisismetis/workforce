ae8b3372a00f1bb980aa22f3bb8b702c6a6f44f3
John  Mazzoli <johnmazzoli@Macintosh-2.local>
2014-04-14 10:59:44 -0400

    rails new workforce -D postgresql -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Workforce::Application.config.secret_key_base = '3e6d00a6528ee58181ab548090c617d5f6aa7181a728373c1e8297b2ac1d9998a07ac24916bd9af84f7a161b7860847d27289b1841faf403af31c0eb126b8b9d'
