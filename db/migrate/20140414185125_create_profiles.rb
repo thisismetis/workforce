e8786f3b7d82ecfa905a05ec945e0235ce772286
John Mazzoli <jmazzoli@gmail.com>
2014-04-14 16:59:48 -0400

    fixed syntax null false error

--- db/migrate/20140414185125_create_profiles.rb
+++ db/migrate/20140414185125_create_profiles.rb
@@ -3,8 +3,8 @@ class CreateProfiles < ActiveRecord::Migration
     create_table :profiles do |t|
       t.belongs_to :user, index: true
       t.text :about
-      t.string :picture_url, null: :false
-      t.string :name, null: :false
+      t.string :picture_url, null: false
+      t.string :name, null: false
       t.date :start_date
       t.date :birthday
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140414185125) do
+ActiveRecord::Schema.define(version: 20140414174009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "profiles", force: true do |t|
-    t.integer  "user_id"
-    t.text     "about"
-    t.string   "picture_url"
-    t.string   "name"
-    t.date     "start_date"
-    t.date     "birthday"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.datetime "created_at",                     null: false
