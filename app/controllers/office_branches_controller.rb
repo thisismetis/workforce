69c8c739497bc22696d57d88ab62e30d6172e224
jsalig1 <jeremy.salig@gmail.com>
2014-04-15 15:42:07 -0400

    trailing whitspace removed

--- app/controllers/office_branches_controller.rb
+++ app/controllers/office_branches_controller.rb
@@ -2,11 +2,11 @@ class OfficeBranchesController < ApplicationController
   def index
     @office_branches = OfficeBranch.all
   end
-  
+
   def new
     @office_branch = OfficeBranch.new
   end
-  
+
   def create
     @office_branch = OfficeBranch.new(office_branch_params)
     if @office_branch.save
@@ -15,15 +15,15 @@ class OfficeBranchesController < ApplicationController
       render :new
     end
   end
-  
+
   def show
     @office_branch = find_office_branch
   end
-  
+
   def edit
     @office_branch = find_office_branch 
   end
-  
+
   def update
     @office_branch = find_office_branch
     if @office_branch.update(office_branch_params)
@@ -32,19 +32,19 @@ class OfficeBranchesController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     office_branch = find_office_branch
     office_branch.destroy
     redirect_to office_branches_path
   end
-  
+
   private
-  
+
   def office_branch_params
     params.require(:office_branch).permit(:location)
   end
-  
+
   def find_office_branch
     OfficeBranch.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Workforce::Application.routes.draw do
   root "homes#show"
 
   resources :profiles
-  
+
   resources :departments, only: [:index, :new, :create, :show]
-  
+
   resources :office_branches
 end
