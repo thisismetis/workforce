761bdb81009f1a3aad8e986050c7cdbe5a5d44af
jsalig1 <jeremy.salig@gmail.com>
2014-04-15 10:43:01 -0400

    Create departments, view department page, view department index.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Workforce::Application.routes.draw do
   root "homes#show"
 
   resources :profiles, only: [:show, :index, :new, :create]
+  
+  resources :departments, only: [:index, :new, :create, :show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140414185125) do
+ActiveRecord::Schema.define(version: 20140415134340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "departments", force: true do |t|
+    t.string   "name",        null: false
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "profiles", force: true do |t|
     t.integer  "user_id"
@@ -30,13 +37,13 @@ ActiveRecord::Schema.define(version: 20140414185125) do
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.datetime "created_at",                                     null: false
-    t.datetime "updated_at",                                     null: false
-    t.string   "email",                                          null: false
-    t.string   "encrypted_password", limit: 128,                 null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "email",                          null: false
+    t.string   "encrypted_password", limit: 128, null: false
     t.string   "confirmation_token", limit: 128
-    t.string   "remember_token",     limit: 128,                 null: false
-    t.boolean  "admin",                          default: false
+    t.string   "remember_token",     limit: 128, null: false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
