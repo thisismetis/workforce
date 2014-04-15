6faaa2f20d8bbc71c464bbf2c4859807a67c7391
John Mazzoli <jmazzoli@gmail.com>
2014-04-15 14:51:36 -0400

    Added name to users, deleted name from profiles

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,7 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  def name
+    user.name
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140415134340) do
+ActiveRecord::Schema.define(version: 20140415184452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 20140415134340) do
     t.integer  "user_id"
     t.text     "about"
     t.string   "picture_url", null: false
-    t.string   "name",        null: false
     t.date     "start_date"
     t.date     "birthday"
     t.datetime "created_at"
@@ -37,16 +36,18 @@ ActiveRecord::Schema.define(version: 20140415134340) do
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "email",                          null: false
-    t.string   "encrypted_password", limit: 128, null: false
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+    t.string   "email",                                          null: false
+    t.string   "encrypted_password", limit: 128,                 null: false
     t.string   "confirmation_token", limit: 128
-    t.string   "remember_token",     limit: 128, null: false
-    t.boolean  "admin"
+    t.string   "remember_token",     limit: 128,                 null: false
+    t.boolean  "admin",                          default: false
+    t.string   "name",               limit: 50,                  null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["name"], name: "index_users_on_name", using: :btree
   add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
