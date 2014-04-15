4c317de0ce3b0c6d43a271665ca180f9c0e45fc4
John Mazzoli <jmazzoli@gmail.com>
2014-04-15 10:56:16 -0400

    Add comments back

--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,21 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20140414185125) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
   create_table "profiles", force: true do |t|
     t.integer  "user_id"
     t.text     "about"
@@ -11,7 +26,9 @@ ActiveRecord::Schema.define(version: 20140414185125) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
+
   create_table "users", force: true do |t|
     t.datetime "created_at",                                     null: false
     t.datetime "updated_at",                                     null: false
@@ -21,6 +38,8 @@ ActiveRecord::Schema.define(version: 20140414185125) do
     t.string   "remember_token",     limit: 128,                 null: false
     t.boolean  "admin",                          default: false
   end
+
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
   add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
+
 end
