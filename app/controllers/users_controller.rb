4083d07f442209251834e003ae47baf5f71d4687
John Mazzoli <jmazzoli@gmail.com>
2014-04-15 15:39:16 -0400

    refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,18 @@
 class UsersController < Clearance::UsersController
   def create
-    @user = user_from_params
+    @user = User.new(user_params)
 
     if @user.save
       sign_in @user
-      redirect_to '/'
+      redirect_to :root
     else
-      render template: 'users/new'
+      render :new
     end
   end
 
   private
 
-  def user_from_params
-    user_params = params[:user] || Hash.new
-    name = user_params.delete(:name)
-    email = user_params.delete(:email)
-    password = user_params.delete(:password)
-
-    Clearance.configuration.user_model.new(user_params).tap do |user|
-      user.name = name
-      user.email = email
-      user.password = password
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
