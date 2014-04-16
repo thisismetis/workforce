af9585c9571ab987863cb9a40cdd6ba45ca4253f
John Mazzoli <jmazzoli@gmail.com>
2014-04-16 11:01:47 -0400

    hound changes

--- app/controllers/contact_informations_controller.rb
+++ app/controllers/contact_informations_controller.rb
@@ -8,15 +8,16 @@ class ContactInformationsController < ApplicationController
   end
 
   def create
-    @contact_information = current_user.create_contact_information(contact_information_params)
+    @contact_information = current_user.
+      create_contact_information(contact_information_params)
     redirect_to @contact_information
   end
 
   private
 
   def contact_information_params
-      params.require(:contact_information).
-        permit(:address, :phone_number, :emergency_name, :emergency_number, :emergency_relation)
+    params.require(:contact_information).
+      permit(:address, :phone_number, :emergency_name, :emergency_number, :emergency_relation)
   end
 
   def find_contact_information
