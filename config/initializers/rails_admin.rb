ae13e51a0da1d5525f5eb0ce782cb8cbd4623923
Jesse Waites <jesse.waites@gmail.com>
2014-04-16 17:14:24 -0400

    indentation prefixes

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -12,9 +12,11 @@ RailsAdmin.config do |config|
   # config.authorize_with :cancan
 
   ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
+  # config.audit_with :paper_trail, 
+  #'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
 
-  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
+  ### More at https://github.com/sferik/
+  #rails_admin/wiki/Base-configuration
 
   config.actions do
     dashboard                     # mandatory
