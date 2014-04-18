4636148561aeaf970858bdf0751633d34a4ca184
jsalig1 <jeremy.salig@gmail.com>
2014-04-18 15:34:06 -0400

    fix trailing whitspace

--- app/controllers/behavior_reports_controller.rb
+++ app/controllers/behavior_reports_controller.rb
@@ -20,7 +20,7 @@ class BehaviorReportsController < ApplicationController
   def edit
     @behavior_report = find_behavior_report
   end
-  
+
   def update
     behavior_report = find_behavior_report
     if behavior_report.update(behavior_report_params)
@@ -29,7 +29,7 @@ class BehaviorReportsController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     behavior_report = find_behavior_report
     behavior_report.destroy
@@ -37,7 +37,7 @@ class BehaviorReportsController < ApplicationController
   end
 
   private
-  
+
   def behavior_report_params
     params.require(:behavior_report).
     permit(:description, :action_taken, :date)
