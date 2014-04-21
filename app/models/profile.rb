4ebaafb3995334aaffac2180247f4cf68008e2e0
John Mazzoli <jmazzoli@gmail.com>
2014-04-21 14:31:51 -0400

    -mtypos

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,5 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
-  validate :avatar, presence: :true
   has_attached_file :avatar
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
