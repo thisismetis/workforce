65ac97dd7edf2a64d8c46f2a88bb6ee0a32481f0
John Mazzoli <jmazzoli@gmail.com>
2014-04-21 16:32:22 -0400

    Add null avatar

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,6 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :avatar
+  has_attached_file :avatar, default_url: :default_avatar
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
@@ -10,5 +10,11 @@ class Profile < ActiveRecord::Base
 
   def present?
     true
+  end
+
+  private
+
+  def default_avatar
+    "/employee-avatar.png"
   end
 end
